89bb8a9a5b81b69f4d30dbe0303f9dab6bc70dc8
Brayan Rivera Gomez <100776460+Brayan883@users.noreply.github.com>
2022-05-10 21:08:15 -0500

    accesibilidad

---
+++ main.js
@@ -0,0 +1,31 @@
+let classes = ["f0", "f1", "f2", "f3", "f4"];
+let classIndex = 2;
+
+document.getElementById('aumentar').addEventListener('click', ()=>{
+    let previousClass = classIndex;
+    classIndex++;
+    classIndex = (classIndex== classes.length) ? classes.length - 1: classIndex;
+    changeClass(previousClass, classIndex ); 
+});
+
+document.getElementById('disminuir').addEventListener('click', ()=>{
+    let previousClass = classIndex;
+    classIndex--;
+    classIndex = (classIndex < 0) ? 0 : classIndex;
+    changeClass(previousClass, classIndex);
+});
+
+let changeClass = (previous, next)=>{
+    if(previous != next){
+        let htmlElement = document.querySelector('html');
+        htmlElement.classList.remove(classes[previous]);
+        htmlElement.classList.add(classes[next]);
+        
+    }
+}
+changeClass()
+const boton = document.getElementById('dark');
+const html = document.body;
+boton.addEventListener('click', ()=>{
+    html.classList.toggle('dark-mode');
+});
